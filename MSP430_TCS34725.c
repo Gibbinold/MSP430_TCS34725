0151fa7fa2239a48c57a3add14f5b428c7761ddf
Gibbinold <joe.gibson7@gmail.com>
2018-03-16 18:10:11 +0000

    Create MSP430_TCS34725.c

---
+++ MSP430_TCS34725.c
@@ -0,0 +1,13 @@
+/**************************************************************************
+
+    @file     MSP430_TCS34725.c
+    @author   Joe Gibson
+    
+    Driver for the TCS34725 digital color sensors. 
+    (Written in C)
+
+    v0.1 - First attempt
+
+**************************************************************************/
+
+#include "MSP430_TCS34725.h"
